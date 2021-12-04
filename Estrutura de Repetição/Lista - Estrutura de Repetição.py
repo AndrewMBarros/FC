fc651850550a9b085964b15e1312bf4c4a7a4af6
andrewmbarros <andrew-barros@hotmail.com>
2021-12-04 13:27:06 -0300

    Estrutura de Decisão

--- Estrutura de Repetição/Lista - Estrutura de Repetição.py
+++ Estrutura de Repetição/Lista - Estrutura de Repetição.py
@@ -25,4 +25,39 @@ nota = notas()
 mensagem (nota)
 
 # ------------------------------------------------- -------------------------------------------------- ---------- #
+2 - Faça um programa que leia um nome de usuário
+    e a sua senha e não aceite a senha igual ao nome do usuário,
+    mostrando uma mensagem de erro e voltando a pedir as informações
 
+def leitura ():
+    #leia um nome de usuário
+    #e a sua senha
+
+    nome = str(input('Digite o nome do usuário: '))
+    senha = str(input('Digite a senha do usuário: '))
+
+    return nome, senha
+
+def analise (senha, nome):
+    while True:
+
+        #não aceite a senha igual ao nome do usuário,
+        if senha == nome:
+
+            #mostrando uma mensagem de erro
+            print(f'A senha é o mesmo nome do usuário. Digite novamente!')
+
+            #voltando a pedir as informações
+            nome = str(input('Digite o nome do usuário: '))
+            senha = str(input('Digite a senha do usuário: '))
+            
+        else:
+            break
+    print(f'Você digitou corretamente. A senha é diferente do usuário!')
+
+    return
+
+nome, senha = leitura ()
+analise (senha, nome)
+
+# ------------------------------------------------- -------------------------------------------------- ---------- #
